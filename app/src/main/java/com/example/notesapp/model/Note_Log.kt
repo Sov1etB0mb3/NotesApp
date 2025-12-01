6ab7f1495c1ceb44824151c298e397075090378d
Alan <nguyentrungkhanh804@gmail.com>
2025-12-01 20:05:43 +0700

    add note_log

---
+++ app/src/main/java/com/example/notesapp/model/Note_Log.kt
@@ -0,0 +1,19 @@
+package com.example.notesapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+import java.util.UUID
+
+@Serializable
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+    val userId:Int=-1,
+    val title: String = "",
+    val content: String = "",
+    val category: String = "Chưa phân loại",
+    val backgroundColor: Long = 0xFFFFFFFF,
+    val isPinned: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis()
+)
